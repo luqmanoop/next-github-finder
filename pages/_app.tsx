8246d3c1ca0b79010a0944a79abea2369c1c4128
Luqman Olushi <olushilukmon03@gmail.com>
2021-12-27 17:36:02 +0100

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
