b41b282b23a7e53528b20d90fa231a363e3ae77a
Luqman Olushi <olushilukmon03@gmail.com>
2021-12-27 17:03:45 +0100

    render user component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from "next";
+import { SyntheticEvent, useRef, useState } from "react";
+
+import User from "../components/User";
 
 const Home: NextPage = () => {
-  return <div>hello, world</div>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [username, setUsername] = useState("octocat");
+
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const ghUsername = inputRef.current?.value;
+    if (!ghUsername?.trim()) return;
+    setUsername(ghUsername);
+    e.currentTarget.reset();
+  };
+
+  return (
+    <div>
+      <User username={username} />
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+          type="search"
+          placeholder="enter github username e.g. octocat"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
